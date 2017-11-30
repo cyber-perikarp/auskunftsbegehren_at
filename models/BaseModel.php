2540d1b69630a6cf4a249b07edf4cb28e11c6022
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2017-11-30 13:49:23 +0100

    Timestamp Behaviour not needed

--- models/BaseModel.php
+++ models/BaseModel.php
@@ -5,19 +5,5 @@ use yii\behaviors\TimestampBehavior;
 use yii\db\Expression;
 
 class BaseModel extends \yii\db\ActiveRecord {
-	use SoftDeleteTrait;
 
-	public function behaviors()
-	{
-		return [
-			[
-				'class' => TimestampBehavior::className(),
-				'value' => new Expression('NOW()'),
-			],
-		];
-	}
-
-	public static function find() {
-		return new BaseQuery(get_called_class());
-	}
 }
