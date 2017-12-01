c11294666cda74f3fa743ec4ce3f00fe089c0b4c
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2017-12-01 09:48:04 +0100

    Ausweistypen

---
+++ models/Auskunft.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+class Auskunft extends Model {
+    public $firstName;
+    public $lastName;
+    public $street;
+    public $streetNumber;
+    public $zip;
+    public $city;
+    
+    public $email;
+    public $additional;
+    public $reminder;
+    public $idType;
+
+    public $targets;
+
+    public function rules()
+    {
+        return [
+            [['firstName', 'lastName', 'street', 'streetNumber', 'zip', 'city'], 'required', 'string', 'max' => 64],
+            [['additional'], 'string', 'max' => 256],
+            ['email', 'email'],
+            ['reminder', 'default', 'value' => false],
+        ];
+    }
+}
