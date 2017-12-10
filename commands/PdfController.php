6754a585bc6c4472791d53142522427190b0261b
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2017-12-10 15:28:38 +0100

    pdf generation works

--- commands/PdfController.php
+++ commands/PdfController.php
@@ -1,19 +1,19 @@
 <?php
 namespace app\commands;
+use app\models\IdTypes;
 use yii\console\Controller;
 use app\models\Auskunft;
 use app\models\Reminders;
 use app\models\Adressdaten;
 use app\models\Statistik;
 use Ramsey\Uuid\Uuid;
-use AdamBrett\ShellWrapper\Command;
-use AdamBrett\ShellWrapper\Command\Param;
-use AdamBrett\ShellWrapper\Runners\Exec;
 
 class PdfController extends Controller
 {
     private $allDatasets;
     private $statistik;
+    private $error; // Die Variable wird true wenn pro Nutzer ein kritischer Fehler auftritt aka ein pdf konnte nicht generiert werden oder das zippen hat nicht funktioniert
+
     public function __construct($id, $module, $config = array()) {
         $this->allDatasets = Auskunft::find()->all();
         parent::__construct($id, $module, $config);
@@ -22,26 +22,40 @@ class PdfController extends Controller
     public function actionIndex()
     {
         foreach ($this->allDatasets as $dataSet) {
+        	\Yii::trace("Processing entry for " . $dataSet["email"]);
+        	$this->error = false;
+
         	$targets = json_decode($dataSet["targets"]);
 			$targetFolder = $this->createFolder();
 
+			$dataSet["idType"] = IdTypes::findOne(["id" => $dataSet["idType"]])["nameForText"];
+
             foreach ($targets as $target) {
+            	\Yii::info("Target: " . $target);
             	$this->saveStats($target);
-				$this->generatePdf(Adressdaten::findOne(["id" => $target]), $dataSet, $targetFolder);
+				if (!$this->generatePdf(Adressdaten::findOne(["id" => $target]), $dataSet, $targetFolder)) {
+					$this->error = true;
+				}
             }
 
+            // Die Logdateien sollen nur gelöscht und die Erinnerungen nur gespeichert werden wenn die pdf generierung erfolgreich war.
+            if (!$this->error) {
+            	\Yii::trace("Deleting log files from generation");
+				$this->deleteTempFiles($targetFolder);
 
-            if($dataSet["reminder"]) {
-            	$this->addReminder($dataSet["email"], $dataSet["targets"]);
-            }
+				if($dataSet["reminder"]) {
+					$this->addReminder($dataSet["email"], $dataSet["targets"]);
+				}
+			} else {
+            	\Yii::error("Oh noes!");
+			}
         }
     }
 
 	private function addReminder($email, $targets) {
+		\Yii::info("Adding Reminder");
     	$now = new \DateTime();
     	$reminder = new Reminders();
-
-
 
     	$reminder->created_at = $now->format("Y-m-d");
     	$reminder->due_at = $now->add(
@@ -54,6 +68,7 @@ class PdfController extends Controller
 	}
 
 	private function saveStats($target) {
+		\Yii::info("Saving Statistik");
 		$this->statistik = new Statistik();
 		$targetEntry = Statistik::findOne(["identifier" => $target]);
 		if (!$targetEntry) {
@@ -66,8 +81,25 @@ class PdfController extends Controller
 		}
 	}
 
+	private function deleteTempFiles ($targetFolder) {
+		// Alle von Latex generierten Daten löschen außer die PDFs. Mehrere foreach because fuck it
+		$extensionsToDelete = array("aux", "log", "lco", "tex");
+
+		// Weil wir nur bestimmte Endungen und das nicht rekursiv löschen ist die Gefahr gering, aber lieber zu viel Vorsicht
+		if ($targetFolder) {
+			foreach ($extensionsToDelete as $extension) {
+				foreach( glob($targetFolder . "/*." . $extension) as $file ) {
+					unlink($file);
+				}
+
+			}
+		}
+
+	}
+
 	private function createFolder() {
 		$targetFolder = \Yii::$app->params["outputBaseDir"] . "/" . $this->generateHash();
+		\Yii::info("Target Folder: " . $targetFolder);
 		mkdir($targetFolder);
 		copy(\Yii::$app->params["baseDir"] . "/templates/brief/base.tex", $targetFolder . "/base.tex");
 
@@ -78,7 +110,28 @@ class PdfController extends Controller
 		return hash("sha512", \Yii::$app->params["salt"] . Uuid::uuid4()->toString());
 	}
 
+	private function generateFilename($oldName) {
+		$umlauts = array(
+			"ö" => "oe",
+			"ä" => "ae",
+			"ü" => "ue",
+			"Ö" => "oe",
+			"Ä" => "ae",
+			"Ü" => "ue",
+			"ß" => "ss"
+		);
+
+		$nameLowercase = strtolower($oldName);
+		$nameWithoutMarks = preg_replace("#[[:punct:]]#", "", $nameLowercase);
+		$nameWithoutSpaces = str_replace(" ", "_", $nameWithoutMarks);
+		$nameWithoutUmlauts = str_replace(array_keys($umlauts), $umlauts, $nameWithoutSpaces);
+
+		return $nameWithoutUmlauts;
+
+	}
+
 	private function generatePdf ($target, $dataSet, $targetFolder) {
+    	$filename = $this->generateFilename($target["name"]);
     	$templateFile = $targetFolder . "/template.lco";
 
 		copy(\Yii::$app->params["baseDir"] . "/templates/brief/template.lco", $templateFile);
@@ -91,6 +144,12 @@ class PdfController extends Controller
 		$template = str_replace("@@hausnummer@@", $dataSet["streetNumber"], $template);
 		$template = str_replace("@@plz@@", $dataSet["zip"], $template);
 		$template = str_replace("@@ort@@", $dataSet["city"], $template);
+		$template = str_replace("@@ausweistext@@", $dataSet["idType"], $template);
+
+		// Hier ersetzen wir einen Parameter durch eine ganze Funktion
+		if($dataSet["additional"]) {
+			$template = str_replace("%@@zusatzinfo@@", "\\newcommand{\zusatzinfo}{" . $dataSet["additional"] . "}", $template);
+		}
 
 		// Daten des Zieles
 		$template = str_replace("@@zielname@@", $target["name"], $template);
@@ -101,14 +160,15 @@ class PdfController extends Controller
 		unlink($templateFile);
 		file_put_contents($templateFile, $template);
 
-		$shell = new Exec();
-		$command = new Command('pdflatex');
-		$command->addParam(new Param("-output-directory " . $targetFolder));
-		$command->addParam(new Param("-interaction batchmode"));
-		$command->addParam(new Param($targetFolder . "/base.tex"));
+		$command = "pdflatex --output-directory " . $targetFolder . " --interaction batchmode --jobname " . $filename . " " . $targetFolder . "/base.tex";
 
-		var_dump($shell->run($command));
-		die();
+		exec($command, $output, $returnCode);
 
+		\Yii::trace("Command pdflatex returned error code " . $returnCode);
+
+		if ($returnCode == 0) {
+			return true;
+		}
+		return false;
 	}
 }
