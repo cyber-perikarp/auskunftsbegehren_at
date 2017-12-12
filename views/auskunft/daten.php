0266230c66f2463e7a85cc6cdf0303a66706bbfb
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2017-12-12 15:30:00 +0100

    first draft of ugly form

--- views/auskunft/daten.php
+++ views/auskunft/daten.php
@@ -2,7 +2,7 @@
 
 use yii\helpers\Html;
 use yii\helpers\ArrayHelper;
-use yii\widgets\ActiveForm;
+use yii\bootstrap\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Auskunft */
@@ -20,39 +20,39 @@ $this->title = "Generieren";
         <?= $form->field($model, 'zip') ?>
         <?= $form->field($model, 'city') ?>
         <?= $form->field($model, 'idType')->dropDownList(ArrayHelper::map($idTypes, 'id', 'name')); ?>
-        <?= $form->field($model, 'additional') ?>
+        <?= $form->field($model, 'additional')->textarea() ?>
 
         <?= $form->field($model, 'email') ?>
         <?= $form->field($model, 'reminder')->checkbox() ?>
 
 
     <table>
-    <?php
-    foreach ($branchen as $branche) {
-	    echo "<tr><td></td><td><h2>" . $branche["branche"] . "</h2></td><td></td><td></td></tr>";
-	    foreach ($ziele as $ziel) {
-		    if($ziel["branche"] == $branche["branche"]) {
-			    echo '
-                <tr>
-                    <td>
-                        <input type="checkbox" name="Auskunft[targets][]" value="' . $ziel["id"] . '" id="' . $ziel["id"] . '">
-                    </td>
-                   
-                    <td>
-                         <label for="' . $ziel["id"] . '">' . $ziel["typ"] . '</label>
-                    </td>
-                    <td>
-                         <label for="' . $ziel["id"] . '">' . $ziel["name"] . '</label>
-                    </td>
-                    <td>
-                         <label for="' . $ziel["id"] . '">' . $ziel["stadt"] . '</label>
-                    </td>
-                    </tr>';
-		    }
-	    }
-    }
-    ?>
-</table>
+        <?php
+        foreach ($branchen as $branche) {
+            echo "<tr><td></td><td><h2>" . $branche["branche"] . "</h2></td><td></td><td></td></tr>";
+            foreach ($ziele as $ziel) {
+                if($ziel["branche"] == $branche["branche"]) {
+                    echo '
+                    <tr>
+                        <td>
+                            <input type="checkbox" name="Auskunft[targets][]" value="' . $ziel["id"] . '" id="' . $ziel["id"] . '">
+                        </td>
+                       
+                        <td>
+                             <label for="' . $ziel["id"] . '">' . $ziel["typ"] . '</label>
+                        </td>
+                        <td>
+                             <label for="' . $ziel["id"] . '">' . $ziel["name"] . '</label>
+                        </td>
+                        <td>
+                             <label for="' . $ziel["id"] . '">' . $ziel["stadt"] . '</label>
+                        </td>
+                        </tr>';
+                }
+            }
+        }
+        ?>
+    </table>
 
         <div class="form-group">
             <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
