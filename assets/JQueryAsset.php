af9da9552a2ad19bd490da703d3445fab6d50937
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2017-12-15 13:57:37 +0100

    use own bootstrap and jquery

--- assets/JQueryAsset.php
+++ assets/JQueryAsset.php
@@ -8,6 +8,7 @@
 namespace app\assets;
 
 use yii\web\AssetBundle;
+use yii\web\View;
 
 /**
  * Main application asset bundle.
@@ -15,20 +16,14 @@ use yii\web\AssetBundle;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class AppAsset extends AssetBundle
+class JQueryAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
-    public $css = [
-        'css/bootstrap.min.css',
-        'css/site.css',
-    ];
+    public function init() {
+        $this->jsOptions['position'] = View::POS_BEGIN;
+        parent::init();
+    }
 
     public $js = [
-        'js/bootstrap.min.js'
-    ];
-    public $depends = [
-        'app\assets\JQueryAsset',
-        'yii\web\YiiAsset'
+        'js/jquery-3.2.1.min.js'
     ];
 }
--- config/web.php
+++ config/web.php
@@ -70,21 +70,21 @@ $config = [
     'params' => $params,
 ];
 
-//if ($dev) {
-//    // configuration adjustments for 'dev' environment
-//    $config['bootstrap'][] = 'debug';
-//    $config['modules']['debug'] = [
-//        'class' => 'yii\debug\Module',
-//        // uncomment the following to add your IP if you are not connecting from localhost.
-//        //'allowedIPs' => ['127.0.0.1', '::1'],
-//    ];
-//
-//    $config['bootstrap'][] = 'gii';
-//    $config['modules']['gii'] = [
-//        'class' => 'yii\gii\Module',
-//        // uncomment the following to add your IP if you are not connecting from localhost.
-//        //'allowedIPs' => ['127.0.0.1', '::1'],
-//    ];
-//}
+if ($dev) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
 
 return $config;
