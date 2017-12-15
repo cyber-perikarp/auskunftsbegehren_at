0cae1479aeb225b35c19f741fef06d742ed13267
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2017-12-15 11:58:49 +0100

    code cleanup

--- views/auskunft/daten.php
+++ views/auskunft/daten.php
@@ -69,18 +69,20 @@ $this->title = "Generieren";
 
 	<?php
 	$i=0;
-	foreach ($branchen as $branche) { ?>
+	foreach ($branchen as $branche):
+        $i++;
+    ?>
 		<div id="ziele" role="tablist">
 			<div class="card">
 				<div class="card-body">
-					<div class="card-header" role="tab" id="category<?php echo $i; ?>">
+					<div class="card-header" role="tab" id="category<?= $i; ?>">
 						<h2 class="mb-0 card-title">
-							<a data-toggle="collapse" href="#collapse<?php echo $i; ?>" aria-expanded="false" aria-controls="collapse<?php echo $i; ?>">
-								<?php echo($branche["branche"]); ?>
+							<a data-toggle="collapse" href="#collapse<?= $i; ?>" aria-expanded="false" aria-controls="collapse<?= $i; ?>">
+								<?= $branche["branche"]; ?>
 							</a>
 						</h2>
 					</div>
-					<div id="collapse<?php echo $i; ?>" class="collapse list-of-targets" role="tabpanel" aria-labelledby="category<?php echo $i; ?>" data-parent="#ziele">
+					<div id="collapse<?= $i; ?>" class="collapse list-of-targets" role="tabpanel" aria-labelledby="category<?= $i; ?>" data-parent="#ziele">
 						<div class="card-text">
 							<table class="table table-striped table-responsive">
 								<tr>
@@ -98,26 +100,26 @@ $this->title = "Generieren";
 									</th>
 								</tr>
 
-								<?php foreach ($ziele as $ziel) {
-									if($ziel["branche"] == $branche["branche"]) { ?>
+								<?php foreach ($ziele as $ziel):
+									if($ziel["branche"] == $branche["branche"]): ?>
 									<tr>
 										<td>
-											<input type="checkbox" name="Auskunft[targets][]" value="<?php echo($ziel["id"]); ?>" id="<?php echo($ziel["id"]); ?>">
+											<input type="checkbox" name="Auskunft[targets][]" value="<?= $ziel["id"]; ?>" id="<?= $ziel["id"]; ?>">
 										</td>
 
 										<td>
-											 <label for="<?php echo($ziel["id"]); ?>"><?php echo($ziel["typ"]); ?></label>
+											 <label for="<?= $ziel["id"]; ?>"><?= $ziel["typ"]; ?></label>
 										</td>
 										<td>
-											 <label for="<?php echo($ziel["id"]); ?>"><?php echo($ziel["name"]); ?></label>
+											 <label for="<?= $ziel["id"]; ?>"><?= $ziel["name"]; ?></label>
 										</td>
 										<td>
-											 <label for="<?php echo($ziel["id"]); ?>"><?php echo($ziel["stadt"]); ?>, <?php echo($ziel["bundesland"]); ?></label>
+											 <label for="<?= $ziel["id"]; ?>"><?= $ziel["stadt"]; ?>, <?= $ziel["bundesland"]; ?></label>
 										</td>
 									</tr>
 									<?php
-									}
-								}
+									endif;
+								endforeach;
 							?>
 							</table>
 						</div>
@@ -125,10 +127,7 @@ $this->title = "Generieren";
 				</div>
 			</div>
 		</div>
-	<?php
-		$i++;
-	}
-	?>
+	<?php endforeach; ?>
 
 
 	<div class="form-group">
