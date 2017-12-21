3b3cf54f427ad2e52ff8716cba2085b53000863a
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2017-12-21 15:00:25 +0100

    latex escape

--- commands/GenerationController.php
+++ commands/GenerationController.php
@@ -24,6 +24,10 @@ class GenerationController extends Controller
 	    $allDatasets = Auskunft::find()->all();
 
         foreach ($allDatasets as $dataSet) {
+            foreach ($dataSet as $key => $value) {
+                $dataSet[$key] = $this->latexEscape($value);
+            }
+
         	\Yii::trace("Processing entry for " . $dataSet["email"]);
         	$this->error = false;
 
@@ -197,8 +201,43 @@ class GenerationController extends Controller
 		$nameWithoutUmlauts = str_replace(array_keys($umlauts), $umlauts, $nameWithoutSpaces);
 
 		return $nameWithoutUmlauts;
-
 	}
+
+	private function latexEscape ($text) {
+	    // https://github.com/mike42/web-pdf/blob/master/LatexTemplate.php
+        // Prepare backslash/newline handling
+        $text = str_replace("\n", "\\\\", $text); // Rescue newlines
+        $text = preg_replace('/[\x00-\x1F\x7F-\xFF]/', '', $text); // Strip all non-printables
+        $text = str_replace("\\\\", "\n", $text); // Re-insert newlines and clear \\
+        $text = str_replace("\\", "\\\\", $text); // Use double-backslash to signal a backslash in the input (escaped in the final step).
+
+        // Symbols which are used in LaTeX syntax
+        $text = str_replace("{", "\\{", $text);
+        $text = str_replace("}", "\\}", $text);
+        $text = str_replace("$", "\\$", $text);
+        $text = str_replace("&", "\\&", $text);
+        $text = str_replace("#", "\\#", $text);
+        $text = str_replace("^", "\\textasciicircum{}", $text);
+        $text = str_replace("_", "\\_", $text);
+        $text = str_replace("~", "\\textasciitilde{}", $text);
+        $text = str_replace("%", "\\%", $text);
+
+        // Brackets & pipes
+        $text = str_replace("<", "\\textless{}", $text);
+        $text = str_replace(">", "\\textgreater{}", $text);
+        $text = str_replace("|", "\\textbar{}", $text);
+
+        // Quotes
+        $text = str_replace("\"", "\\textquotedbl{}", $text);
+        $text = str_replace("'", "\\textquotesingle{}", $text);
+        $text = str_replace("`", "\\textasciigrave{}", $text);
+
+        // Clean up backslashes from before
+        $text = str_replace("\\\\", "\\textbackslash{}", $text); // Substitute backslashes from first step.
+        $text = str_replace("\n", "\\\\", trim($text)); // Replace newlines (trim is in case of leading \\)
+
+        return $text;
+    }
 
 	private function generatePdf ($target, $dataSet, $targetFolder) {
     	$filename = $this->generateFilename($target["name"]);
