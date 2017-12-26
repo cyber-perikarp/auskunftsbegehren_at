b18ec1e89f624300fcf030d6d7ea23fb4ee310d3
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2017-12-26 23:38:30 +0100

    cleanup controller

--- commands/CleanupController.php
+++ commands/CleanupController.php
@@ -5,5 +5,40 @@ use app\models\Generated;
 
 class CleanupController extends Controller
 {
+	private $now;
 
+	public function __construct(string $id, $module, array $config = []) {
+		$this->now = new \DateTime();
+		parent::__construct($id, $module, $config);
+	}
+
+	public function actionIndex() {
+		$this->delete(
+			Generated::findAll(
+				[
+					"linkopened" => true
+				]
+			)
+		);
+		
+		$this->delete(
+			Generated::find()->where(
+				[
+					'<',
+					'todelete_at',
+					$this->now->format("Y-m-d H:i:s")
+				]
+			)->all()
+		);
+	}
+
+	private function delete($allToDelete) {
+		foreach ($allToDelete as $entry) {
+			try {
+				$entry->delete();
+			} catch (\Exception $e) {
+				\Yii::error("Could not delete entry: " . $e);
+			}
+		}
+	}
 }
