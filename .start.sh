a83148b0440fb4a97035e2c404d106951b791f09
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2018-02-24 14:33:56 +0100

    fixed typo

--- .start.sh
+++ .start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-awk '{sub(/%mailpass%/,"$PASSWORD")}1' /var/www/.env
-awk '{sub(/%dbpass%/,"$MAILPASSWORD")}1' /var/www/.env
+awk '{sub(/%mailpass%/,"$MAILPASSWORD")}1' /var/www/.env
+awk '{sub(/%dbpass%/,"$PASSWORD")}1' /var/www/.env
 
 ./var/www/yii migrate --interactive=0 > /dev/stdout 2>&1
 php-fpm7.1 -D && nginx
