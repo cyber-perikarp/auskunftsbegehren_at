288d9598062430af647e39b28ff5e5daf7cce427
Sebastian Elisa Pfeifer <sebastian.pfeifer@unicorncloud.org>
2018-02-24 14:45:40 +0100

    fixed startup script

--- .start.sh
+++ .start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
-awk '{sub(/%mailpass%/,"$MAILPASSWORD")}1' /var/www/.env
-awk '{sub(/%dbpass%/,"$PASSWORD")}1' /var/www/.env
+sed 's/'%mailpass%'/'"$MAILPASSWORD"'/' /var/www/.env
+sed 's/'%dbpass%'/'"$PASSWORD"'/' /var/www/.env
+sed 's/'%salt%'/'"$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)"'/' /var/www/.env
+
 
 ./var/www/yii migrate --interactive=0 > /dev/stdout 2>&1
 php-fpm7.1 -D && nginx
