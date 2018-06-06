39ed218eb275c5f0b44ec5f18bc1dce2b7c957bc
Daniel Weissengruber <daniel.weissengruber@gmail.com>
2018-06-06 23:25:13 +0200

    first steps

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -46,6 +46,23 @@ class SiteController extends Controller
         return $this->render('about');
     }
 
+    public function actionContact()
+    {
+        $model=new ContactForm;
+		if(isset($_POST['ContactForm']))
+		{
+			$model->attributes=$_POST['ContactForm'];
+			if($model->validate())
+			{
+				$headers="From: {$model->email}\r\nReply-To: {$model->email}";
+				mail(Yii::app()->params['email_from'],$model->subject,$model->body,$headers);
+				Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
+				$this->refresh();
+			}
+		}
+		$this->render('contact',array('model'=>$model));
+    }
+
 	public function actionImprint()
 	{
 		return $this->render('imprint');
