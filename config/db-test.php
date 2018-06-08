01bafd82ac8203f0e45b211800d8e051537b5377
Daniel Weissengruber <daniel.weissengruber@gmail.com>
2018-06-08 19:13:17 +0200

    fixed travis pipeline

---
+++ config/db-test.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => "mysql:host=derzer.at;dbname=auskunftsbegehren_at",
+    'username' => "auskunftsbegehren_at",
+    'password' => "auskunftsbegehren_at",
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
--- config/test.php
+++ config/test.php
@@ -1,6 +1,6 @@
 <?php
-$params = require __DIR__ . '/params.php';
-$db = require __DIR__ . '/test_db.php';
+$params = require __DIR__ . '/params-test.php';
+$db = require __DIR__ . '/db-test.php';
 
 /**
  * Application configuration shared by all test types
