388403b9410643d287105b216106c247efab74c4
Daniel Weissengruber <daniel.weissengruber@gmail.com>
2018-06-10 16:47:34 +0200

    bissl hübscher

--- views/site/contact.php
+++ views/site/contact.php
@@ -18,31 +18,42 @@ $this->title = 'Contact';
             Danke für Ihre Nachricht, wir werden uns so bald wie möglich um Ihr anliegen kümmern.
         </div>
     <?php else: ?>
-        <div class="row">
-            <div class="col-lg-5">
-
-                <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
-
-                    <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
-
-                    <?= $form->field($model, 'email') ?>
-
-                    <?= $form->field($model, 'subject') ?>
-
-                    <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
-
-                    <?= $form->field($model, 'verifyCode')->widget(Captcha::class, [
-                        'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
-                    ]) ?>
-
-                    <div class="form-group">
-                        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
-                    </div>
-
-                <?php ActiveForm::end(); ?>
-
+        <?php $form = ActiveForm::begin([
+            'id' => 'contact-form',
+            'layout' => 'horizontal',
+            'fieldConfig' => [
+                'options' => [
+    //				'tag' => false,
+                ],
+                'horizontalCssClasses' => [
+                    'label' => 'col-md-4',
+                    'offset' => 'col-md-offset-2',
+                    'wrapper' => 'col-md-6',
+                    'error' => '',
+                    'hint' => '',
+                ],
+            ],
+        ]); ?>
+        <div class="form-fields container">
+            <div class="form-row">
+                <div class="form-group col-sm-6">
+                <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+                <?= $form->field($model, 'email') ?>
+                <?= $form->field($model, 'subject') ?>
+                </div>
+                <div class="form-group col-sm-6">
+                <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-12">                        
+                <?= $form->field($model, 'verifyCode')->widget(Captcha::class) ?>
+                </div>
             </div>
         </div>
-
+        <div class="form-group">
+		<?= Html::submitButton('Absenden!', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+	    </div>
+        <?php ActiveForm::end(); ?>
     <?php endif; ?>
 </div>
